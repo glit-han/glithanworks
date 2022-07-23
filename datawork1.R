400a129b1eb794f594b1d12ad15d69e4130f977a
Glit Hanpanitkitkan <glithanpanitkitkan@gmail.com>
2022-07-22 19:43:12 -0700

    First commit for game simulation

---
+++ datawork1.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+data<- data.frame(
+  col.1 = 5,
+  col.2 = 6
+)
+
+data2 <- data%>%
+  mutate(
+    col.3 = 7
+  )
+data.add1 <- data2(
+  col.1 = 1,
+)
+  
+ if(weekdays(Sys.Date()) %in% c('Monday', 'Tuesday','Wednesday' 'Thursday', 'Friday')){
+  print('today is a weekday!')
+} else{
+  print('today is a weekend...') 
+}
+
+data_3 <- data.frame(
+  Energy = c(5000,4000,3000,2500,2000,1000,0),
+  HP = c(100,80,60,50,40,20,0)
+)
+data_4 <- data_3%>%
+  mutate(
+    Health = case_when(
+      HP > 50 ~ 'High',
+      HP == 50 ~ 'Medium',
+      HP < 50 & HP > 0~ 'Low',
+      TRUE ~ "Dead")
+)
+    
+ data_5<- data_4%>%
+  mutate(
+    Power = case_when(
+      Energy > 4000 ~ 'High',
+      Energy == 4000 ~ 'Medium',
+      Energy >= 2500 ~'Medium',
+      
+      Energy < 2500 & Energy >0 ~ 'Low',
+      TRUE ~ "Dead"
+      
+    )
+  )
+ 
+ data_6 <- data_5%>%
+   mutate(
+     Play_again = case_when(
+       Energy > 4000 ~ 'Yes Please!',
+       Energy == 4000 ~ 'Ok sure',
+       Energy >= 2500 ~'Sure',
+       
+       Energy < 2500 & Energy > 0~ 'No',
+       TRUE ~ "No please..."
+       
+     )
+   )
+ 
+Data_7 <-  
+ 
